a87c0ce02fa4127c1135fe1549c3ba65e3c96b2a
Will Shayne <wwshayn@lps.umd.edu>
2024-06-20 11:22:21 -0400

    added benchmarks for solovay strassen

--- benches/primality.rs
+++ benches/primality.rs
@@ -1,6 +1,6 @@
 use primality_tests::{
     miller_rabin::miller_rabin, miller_rabin::miller_rabin_list, sieve_primes, trial_division,
-    trial_divisions,
+    trial_divisions, solovay_strassen::solovay_strassen, solovay_strassen::solovay_strassen_list
 };
 
 fn main() {
@@ -22,12 +22,22 @@ fn miller_rabin_test(arg: u64) {
     miller_rabin_list(10, arg);
 }
 
-#[divan::bench(args=[109930813984377167])]
+#[divan::bench(args = [1000, 10000, 100000, 1000000])]
+fn solovay_strassen_list_test(arg: u64) {
+    solovay_strassen_list(10, arg);
+}
+
+#[divan::bench(args=[999999937])]
 fn trial_division_single_test(arg: u64) {
     trial_division(arg);
 }
 
-#[divan::bench(args=[109930813984377167])]
+#[divan::bench(args=[999999937])]
 fn miller_rabin_single_test(arg: u64) {
     miller_rabin(10, arg);
+}
+
+#[divan::bench(args=[999999937])]
+fn solovay_strassen_single_test(arg: u64) {
+    solovay_strassen(10, arg);
 }
