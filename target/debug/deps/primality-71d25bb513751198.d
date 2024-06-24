392799b9f121371ca68dcc0be1f9a98b5b47cff5
otinosan <santinosini@gmail.com>
2024-06-24 13:15:02 -0400

    add ignore

--- target/debug/deps/primality-71d25bb513751198.d
+++ target/debug/deps/primality-71d25bb513751198.d
@@ -1,5 +1,5 @@
-/home/santino/Documents/primality/primality/target/debug/deps/primality-71d25bb513751198: src/main.rs
+/home/santino/Documents/fcsi_rust_lamellar/primality/target/debug/deps/primality-71d25bb513751198: src/main.rs
 
-/home/santino/Documents/primality/primality/target/debug/deps/primality-71d25bb513751198.d: src/main.rs
+/home/santino/Documents/fcsi_rust_lamellar/primality/target/debug/deps/primality-71d25bb513751198.d: src/main.rs
 
 src/main.rs:
