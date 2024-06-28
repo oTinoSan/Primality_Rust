ff31b6b2d9eaf5c337e4360174b09fa9f44deffa
Will Shayne <wwshayn@lps.umd.edu>
2024-06-28 09:22:20 -0400

    added threaded solovay strassen

--- benches/bigint_benchmark.rs
+++ benches/bigint_benchmark.rs
@@ -1,6 +1,6 @@
 use divan::black_box;
 use primality_tests::bigint_algorithms::{
-    miller_rabin, miller_rabin_range, miller_rabin_threaded, solovay_strassen,
+    miller_rabin, miller_rabin_range, miller_rabin_threaded, solovay_strassen, solovay_strassen_range, solovay_strassen_threaded
 };
 use rug::Integer;
 
@@ -21,6 +21,20 @@ fn bigint_miller_rabin_range() {
 #[divan::bench()]
 fn bigint_miller_rabin_threaded_range() {
     miller_rabin_threaded(
+        black_box(10),
+        black_box(Integer::from(1000000)),
+        black_box(4),
+    );
+}
+
+#[divan::bench()]
+fn bigint_solovay_strassen_range() {
+    solovay_strassen_range(black_box(10), black_box(Integer::from(1000000)));
+}
+
+#[divan::bench()]
+fn bigint_solovay_strassen_threaded_range() {
+    solovay_strassen_threaded(
         black_box(10),
         black_box(Integer::from(1000000)),
         black_box(4),
--- src/bigint_algorithms.rs
+++ src/bigint_algorithms.rs
@@ -102,3 +102,34 @@ pub fn miller_rabin_threaded(num_tests: u64, max: Integer, num_threads: u64) -> 
 
     results
 }
+
+pub fn solovay_strassen_threaded(num_tests: u64, max: Integer, num_threads: u64) -> Vec<Integer> {
+    let per_thread = (&max / num_threads).complete();
+    let mut threads = vec![];
+    for i in 0..num_threads {
+        let mut thread_min: Integer = (i * &per_thread).complete() + 5;
+        if Integer::from(&thread_min % 2) == 0 {
+            thread_min += 1;
+        }
+        let thread_max = ((i + 1) * &per_thread).complete();
+        threads.push(thread::spawn(move || {
+            let mut r = vec![];
+            let mut idx = thread_min;
+            while idx < thread_max {
+                if solovay_strassen(num_tests, idx.clone()) {
+                    r.push(idx.clone());
+                }
+                idx += 2;
+            }
+            r
+        }));
+    }
+    let mut results = vec![];
+    for _ in 0..num_threads {
+        let handle = threads.remove(0);
+        let mut thread_results = handle.join().unwrap();
+        results.append(&mut thread_results);
+    }
+
+    results
+}
