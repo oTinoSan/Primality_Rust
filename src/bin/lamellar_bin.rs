b4729eec782bdaab080fd3ebdd22000cadc4e1e2
Will Shayne <wwshayn@lps.umd.edu>
2024-07-02 10:07:48 -0400

    formatting

--- src/bin/lamellar_bin.rs
+++ src/bin/lamellar_bin.rs
@@ -17,9 +17,19 @@ fn main() {
     let local_min = Integer::from(&step * my_pe);
     let local_max = Integer::from(&step * (my_pe + 1));
 
-    let local_results = primality_tests::bigint_algorithms::wheel_threaded::general_wheel_threaded(10, local_min, local_max, 128, primality_tests::bigint_algorithms::miller_rabin, vec![2, 3, 5], vec![1, 7, 11, 13, 17, 19, 23, 29]);
-    let local_results = primality_tests::bigint_algorithms::miller_rabin_range(10, search_max.clone());
-    results.mut_local_data().at(0).store(local_results.len() as u64);
+    let local_results = primality_tests::bigint_algorithms::wheel_threaded::general_wheel_threaded(
+        10,
+        local_min,
+        local_max,
+        128,
+        primality_tests::bigint_algorithms::miller_rabin,
+        vec![2, 3, 5],
+        vec![1, 7, 11, 13, 17, 19, 23, 29],
+    );
+    results
+        .mut_local_data()
+        .at(0)
+        .store(local_results.len() as u64);
 
     world.barrier();
 
