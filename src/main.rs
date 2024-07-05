d243a696de7994f48f4675dea9aaff1bc4955230
otinosan <santinosini@gmail.com>
2024-07-04 22:35:30 -0400

    adds threaded_bigrug to main

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use bigint_num;
 use bigint_rug;
 use num_bigint::BigUint;
 use num_traits::FromPrimitive;
-use primality::*;
+use primality::{bigint_num::miller_rabin_threaded, 
+    bigint_rug::threaded_bigint_rug, *};
 use rug::{ops::Pow, Integer};
 
 fn main() {
@@ -31,49 +32,52 @@ fn main() {
     // let result = solovay_strassen::solovay_strassen(479001599);
     // println!("{:?}", result);
 
-    let num_list: [BigUint; 14] = [
-        BigUint::from(0u8),
-        BigUint::from(1u8),
-        BigUint::from(2u8),
-        BigUint::from(3u8),
-        BigUint::from(4u8),
-        BigUint::from(17u32),
-        BigUint::from(1000u32),
-        BigUint::from(2000u32),
-        BigUint::from(4000u32),
-        BigUint::from(8000u32),
-        BigUint::from(16000u32),
-        BigUint::from(32000u32),
-        BigUint::from(57973u32),
-        BigUint::from(898945346530344442u64),
-    ];
-    for item in num_list {
-        let tested_num = item.clone();
-        let result = bigint_num::miller_rabin_big::miller_rabin_bignum(item, 5);
-        println!("Is {} prime? {}", tested_num, result);
-    }
+    // let num_list: [BigUint; 14] = [
+    //     BigUint::from(0u8),
+    //     BigUint::from(1u8),
+    //     BigUint::from(2u8),
+    //     BigUint::from(3u8),
+    //     BigUint::from(4u8),
+    //     BigUint::from(17u32),
+    //     BigUint::from(1000u32),
+    //     BigUint::from(2000u32),
+    //     BigUint::from(4000u32),
+    //     BigUint::from(8000u32),
+    //     BigUint::from(16000u32),
+    //     BigUint::from(32000u32),
+    //     BigUint::from(57973u32),
+    //     BigUint::from(898945346530344442u64),
+    // ];
+    // for item in num_list {
+    //     let tested_num = item.clone();
+    //     let result = bigint_num::miller_rabin_big::miller_rabin_bignum(item, 5);
+    //     println!("Is {} prime? {}", tested_num, result);
+    // }
 
-    let num_list: [Integer; 14] = [
-        Integer::from(0u8),
-        Integer::from(1u8),
-        Integer::from(2u8),
-        Integer::from(3u8),
-        Integer::from(4u8),
-        Integer::from(17u32),
-        Integer::from(1000u32),
-        Integer::from(2000u32),
-        Integer::from(4000u32),
-        Integer::from(8000u32),
-        Integer::from(16000u32),
-        Integer::from(32000u32),
-        Integer::from(57973u32),
-        Integer::from(898945346530344442u64),
-    ];
-    for item in num_list {
-        let tested_num = item.clone();
-        let result = bigint_rug::miller_rabin_rug::miller_rabin_bigrug(item, 5);
-        println!("Is {} prime? {}", tested_num, result);
-    }
+    // let num_list: [Integer; 14] = [
+    //     Integer::from(0u8),
+    //     Integer::from(1u8),
+    //     Integer::from(2u8),
+    //     Integer::from(3u8),
+    //     Integer::from(4u8),
+    //     Integer::from(17u32),
+    //     Integer::from(1000u32),
+    //     Integer::from(2000u32),
+    //     Integer::from(4000u32),
+    //     Integer::from(8000u32),
+    //     Integer::from(16000u32),
+    //     Integer::from(32000u32),
+    //     Integer::from(57973u32),
+    //     Integer::from(898945346530344442u64),
+    // ];
+    // for item in num_list {
+    //     let tested_num = item.clone();
+    //     let result = bigint_rug::miller_rabin_rug::miller_rabin_bigrug(item, 5);
+    //     println!("Is {} prime? {}", tested_num, result);
+    // }
+
+    let result = threaded_bigint_rug::threaded_bigrug(100, 2);
+    println!("{:?}", result);
 
     //     check_primes_up_to(1_000_000);
     //     pub fn check_primes_up_to(candidate: i64) {
