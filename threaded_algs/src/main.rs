9a74cb8d87721009b22d6092be93179a9ffe5d4b
Beth <eaarlin@lps.umd.edu>
2024-07-11 14:58:28 -0400

    testing larger wheel

--- threaded_algs/src/main.rs
+++ threaded_algs/src/main.rs
@@ -11,8 +11,8 @@ fn main() {
     // threaded_miller_rabin(Integer::from(1000000000), 8);
     // let limit = "100000000";
     // let limit_int = limit.parse::<Integer>().unwrap();
-    lamellar_wheel_miller();
-    // lamellar_wheel_solovay();
+    // lamellar_wheel_miller();
+    lamellar_wheel_solovay();
 }
 
 pub fn threaded_miller_rabin(limit: Integer, num_threads: u64) -> Vec<Integer> {
