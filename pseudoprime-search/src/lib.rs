679775ff99fefc0a86655b31adfa51703884ce95
Will Shayne <wwshayn@lps.umd.edu>
2024-07-24 09:40:06 -0400

    fixed threaded version

--- pseudoprime-search/src/lib.rs
+++ pseudoprime-search/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::{BTreeMap, HashMap}, hash::Hash, sync::mpsc::Sender, thread};
+use std::{collections::{BTreeMap, HashMap}, sync::mpsc::Sender, thread};
 
 use num::integer::{gcd, Roots};
 
@@ -15,46 +15,43 @@ pub fn q_search(max: u64) -> BTreeMap<u64, (u64, u64)> {
     result
 }
 
-fn q_single_thread(mut min: u64, max: u64, global_max: u64, tx: Sender<(u64, (u64, u64))>){
+fn q_single_thread(mut min: u64, max: u64) -> BTreeMap<u64, (u64, u64)>{
+    let mut r = BTreeMap::new();
     if min % 2 == 0 {
         min += 1;
     }
-    for i in (min..=max).step_by(2) {
-        for j in (i..=global_max/i).step_by(2) {
+    for i in (3..=max.sqrt()).step_by(2) {
+        let mut min = min / i;
+        if min % 2 == 0 {
+            min += 1;
+        }
+        for j in (i.max(min)..=max/i).step_by(2) {
             let gcd = gcd(i, j);
             if gcd == 1 {
-                tx.send((i * j, (j, i)));
+                r.insert(i*j, (i, j));
             }
         }
     }
+    r
 }
 
 pub fn q_search_threaded(max: u64, num_threads: u64) -> BTreeMap<u64, (u64, u64)> {
-    let (tx, rx) = std::sync::mpsc::channel();
-    let combiner = thread::spawn(move || {
-        let mut r = BTreeMap::new();
-        for (k, v) in rx {
-            r.insert(k, v);
-        }
-        r
-    });
     let mut handles = vec![];
-    let step = max.sqrt() / num_threads;
+    let step = max / num_threads;
     for i in 0..num_threads {
-        let thread_min = step * i + 3;
+        let thread_min = step * i;
         let mut thread_max = thread_min + step;
         if i == num_threads - 1 {
-            thread_max = max.sqrt() + 1;
+            thread_max = max;
         }
-        let tx = tx.clone();
-        handles.push(thread::spawn(move || q_single_thread(thread_min, thread_max, max, tx)));
+        handles.push(thread::spawn(move || q_single_thread(thread_min, thread_max)));
     }
+
+    let mut res = BTreeMap::new();
 
     for handle in handles {
-        handle.join().unwrap();
+        res.extend(handle.join().unwrap());
     }
 
-    drop(tx);
-    
-    combiner.join().unwrap()
+    res
 }
